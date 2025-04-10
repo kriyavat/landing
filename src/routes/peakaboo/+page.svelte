e93ccd4d60a7ab7cbab12164bf1b07d795bcfe70
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2025-04-10 14:19:15 +0530

    badge added

--- src/routes/peakaboo/+page.svelte
+++ src/routes/peakaboo/+page.svelte
@@ -12,9 +12,14 @@
 	<div class="pt-16">
 		<div class="flex flex-col items-center gap-4 text-center">
 			<img src="/products/peakaboo.png" alt="Peakaboo" class="h-[200px] w-[200px]" />
-			<div>
+			<div class="flex flex-col items-center gap-1">
 				<h1 class="text-2xl font-bold">Peakaboo</h1>
 				<p class="text-gray-500">Preview your Mac camera in a click</p>
+				<!-- badge -->
+				<div class="mt-2 flex items-center gap-2">
+					<p class="w-fit rounded-full bg-blue-50 px-2 py-1 text-xs text-blue-500">macOS</p>
+					<p class="w-fit rounded-full bg-green-50 px-2 py-1 text-xs text-green-500">Free</p>
+				</div>
 			</div>
 			<div>
 				<button class="rounded-full bg-blue-500 px-4 py-2 font-semibold text-white">
